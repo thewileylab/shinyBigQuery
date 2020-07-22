cab8055f8d671c784fb1349888cede98dbe02a79
David Mayer <david.mayer@cuanschutz.edu>
2020-07-22 16:50:37 -0600

    add runtime options - open in a browser (required for Google Auth) - open on port 8100 (Google needs to know where to redirect)

--- R/run_example.R
+++ R/run_example.R
@@ -5,14 +5,12 @@
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
-run_example <- function(
-  ...
-) {
+run_example <- function(...) {
   with_golem_options(
     app = shinyApp(
       ui = app_ui, 
-      server = app_server
-    ), 
+      server = app_server,
+      options = list(port = 8100,launch.browser=T)),
     golem_opts = list(...)
   )
 }
